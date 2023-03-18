eef35e38e456a809fec0e591cefa0806866b6d09
benjrise <benjrise@hotmail.co.uk>
2023-03-18 18:59:21 +0000

    auto-backup

--- .config/nvim/lua/user/markdown-preview.lua
+++
@@ -1,2 +0,0 @@
--- By default the theme is define according to the preferences of the system
-vim.g.mkdp_theme = 'dark'
--- scripts/portable-monitor.sh
+++ scripts/portable-monitor.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-xrandr -output DVI-I-2-1 --auto --right-of eDP --rotate left
+xrandr --output DVI-I-2-1 --auto --right-of eDP --rotate left
